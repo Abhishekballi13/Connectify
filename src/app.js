b4f625814d146540f9ad6a97e4baa182bfe2153f
unknown <ballig360@gmail.com>
2025-02-10 02:54:03 +0530

    auth middleware

--- src/app.js
+++ src/app.js
@@ -2,10 +2,25 @@ const express = require("express");
 
 const app = express();
 
-app.use("/hello",(req,res)=>{
-    res.send("Hello Hello");
+const {adminAuth, userAuth} = require("./middlewares/auth");
+
+//app.use("/route",rH,[rH2,rH3],rH4,rH5);
+// GET /user => midddleware chain => request handler
+
+//Handle Auth middleware for all GET POST,... requests.
+app.use("/admin",adminAuth);
+
+app.get("/user",userAuth,(req,res)=>{
+    res.send("User data send");
 })
 
+ app.get("/admin/getAllData",(req,res)=>{
+    res.send("All Data send");
+ })
+
+ app.get("/admin/deleteUser",(req,res)=>{
+    res.send("Deleted a user");
+ })
 
 //creating server that listens on port 3000
 app.listen(3000,()=>{
