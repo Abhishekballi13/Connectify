75a460f5ddc0fe2f52b820cc31ec30574a06a57c
unknown <ballig360@gmail.com>
2025-02-10 22:40:05 +0530

    created GET apis

--- src/app.js
+++ src/app.js
@@ -7,9 +7,7 @@ const {User} = require("./models/user");
 //it reads the json objeect ,converts it to javascript object add the js object back to req body.
 app.use(express.json());
 
-app.post("/signup",async (req,res)=>{
-    console.log(req.body);
-    
+app.post("/signup",async (req,res)=>{    
     //creating a new instance of this User model
     const user = new User(req.body);
     
@@ -19,6 +17,30 @@ app.post("/signup",async (req,res)=>{
         res.send("User Added successfully")
     }catch(err){
         res.status(400).send("Error saving the user:"+err.message);
+    }
+})
+
+//Get user by email
+app.get("/user",async (req,res)=>{
+    const userEmail = req.body.emailId;
+    try{
+        const users = await User.find({emailId:userEmail});
+        if(users.length===0){
+            res.status(404).send("USer Not Found!!");
+        }
+        else res.send(users);
+    }catch(err){
+        res.status(400).send("Somrthing went wrong!!")
+    }
+})
+
+app.get("/feed",async (req,res)=>{
+    
+    try{
+        const users = await User.find({});
+        res.send(users);
+    }catch(err){
+        res.status(401).send("Something went wrong")
     }
 })
 
