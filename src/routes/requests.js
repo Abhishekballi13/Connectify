a87608508eb96365a341205f7a759569a5c7feaf
unknown <ballig360@gmail.com>
2025-02-22 04:57:02 +0530

    changes done

--- src/routes/requests.js
+++ src/routes/requests.js
@@ -65,7 +65,7 @@ requestRouter.post("/request/send/:status/:toUserId",userAuth,async (req,res) =>
             "A new friend request from " + req.user.firstName,
             req.user.firstName+ " is " + status + " in " + toUser.firstName
         );
-        console.log(emailRes);
+        // console.log(emailRes);
 
         // console.log("Sending a connection request");
         res.json({
