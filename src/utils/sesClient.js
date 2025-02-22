67b5e6195fd23bef5146d2b56a6fa7bf83ffb1ce
unknown <ballig360@gmail.com>
2025-02-22 18:33:31 +0530

    changed environment variables that are reserved

--- src/utils/sesClient.js
+++ src/utils/sesClient.js
@@ -5,8 +5,8 @@ const REGION = "us-east-1";
 
 //Create SES service object.
 const sesClient = new SESClient({region:REGION,credentials:{
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_KEY,
+    accessKeyId: process.env.MY_AWS_ACCESS_KEY,
+    secretAccessKey: process.env.MY_AWS_SECRET_KEY,
 }});
 
 module.exports = {sesClient};
